cbb8153a8e4c30287b86a840bd0bc0243593137a
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2024-10-01 20:58:54 -0600

    change css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const taskInput = document.getElementById('taskInput');
 const taskDate = document.getElementById('taskDate');
 const addTaskButton = document.getElementById('addTaskButton');
 const taskList = document.querySelector('.the-todo-list');
-const loginLogout = document.createElement('button')
+
 addTaskButton.addEventListener('click', () => {
     const taskText = taskInput.value.trim();
     const dueDate = taskDate.value;
