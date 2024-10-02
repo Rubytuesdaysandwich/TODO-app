b4ddbf5d0624a277a0f99cf6f09fd54adaf28cb9
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2024-10-02 17:32:25 -0600

    update

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,14 +1,21 @@
 //This script is for the processing of the signing up of users.
-
+console.log('script loaded');
 const signUpForm = document.querySelector("#signupForm");
 signUpForm.addEventListener('submit', function (e) {
-    e.preventDefault();//prevents the browser from refreshing on form submit loosing the user data 
+    e.preventDefault();//prevents the browser from refreshing on form submit loosing the user data
     console.log('clicked');
     //grabbing the data from the form to be passed
-    const username = document.querySelector("#username").value;//grabbing the value in the input box;
-    const password = document.querySelector("#password").value;
+    const username = document.querySelector("#create-username").value;//grabbing the value in the input box;
+    const password = document.querySelector("#create-password").value;
 
     //here we process the from handeling logic and pass to firebase
+    if (!username || !password) {
+        console.log('pass');
+
+
+        return;//stop form submission if fields are empty
+    }
+
     console.log('Form submitted:', { username, password });
 
 
